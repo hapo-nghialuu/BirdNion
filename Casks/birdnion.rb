8fb15e4463aa056cd3c49d2fd3ff2d4cc268ea1b
hapo-nghialuu <nghialt@haposoft.com>
2026-09-13 16:25:36 +0700

    build(release): bump cask to 0.10.37

--- Casks/birdnion.rb
+++ Casks/birdnion.rb
@@ -1,6 +1,6 @@
 cask "birdnion" do
-  version "0.10.36"
-  sha256 "e86a932f0c879cfc5bfb61197cd307876f781ac3c88c4173381428f8561d5d9f"
+  version "0.10.37"
+  sha256 "dd2c234311ea5aadd5a56b63028aef0f790c57d8f9d7d5201a5400f97fd6d9e7"
 
   url "https://github.com/hapo-nghialuu/BirdNion/releases/download/v#{version.csv.first}/BirdNion-#{version.csv.first}.zip"
   name "BirdNion"
